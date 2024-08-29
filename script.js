d7b1f2902086e2d2ed38604ec1a711e25f64f3ef
Viktor Tóth <tviktor19@gmail.com>
2024-08-29 23:28:38 +0200

    change click to hover

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
+// Click
+
+// let menuBtn = document.getElementById("menuBtn");
+// let sideNav = document.getElementById("sideNav");
+// let menu = document.getElementById("menu");
+
+// sideNav.style.right = "-250px";
+
+// menuBtn.addEventListener("click", () => {
+//     if (sideNav.style.right == "-250px") {
+//         sideNav.style.right = "0";
+//         menuBtn.style.background = "none";
+//         menu.src = "images/close.png";
+//     } else {
+//         sideNav.style.right = "-250px";
+//         menuBtn.style.background = "rgba(165, 199, 181, 0.9)";
+//         menu.src = "images/menu.png";
+//     }
+// });
+
+// document.addEventListener("click", event => {
+//     if (!sideNav.contains(event.target) && !menuBtn.contains(event.target)) {
+//         sideNav.style.right = "-250px";
+//         menuBtn.style.background = "rgba(165, 199, 181, 0.9)";
+//         menu.src = "images/menu.png";
+//     }
+// });
+
+// Hover
 let menuBtn = document.getElementById("menuBtn");
 let sideNav = document.getElementById("sideNav");
 let menu = document.getElementById("menu");
 
 sideNav.style.right = "-250px";
 
-menuBtn.addEventListener("click", () => {
-    if (sideNav.style.right == "-250px") {
-        sideNav.style.right = "0";
-        menuBtn.style.background = "none";
-        menu.src = "images/close.png";
-    } else {
-        sideNav.style.right = "-250px";
-        menuBtn.style.background = "rgba(165, 199, 181, 0.9)";
-        menu.src = "images/menu.png";
-    }
+menuBtn.addEventListener("mouseenter", () => {
+    sideNav.style.right = "0";
+    menuBtn.style.display = "none";
+    menuBtn.style.background = "none";
 });
 
-document.addEventListener("click", event => {
-    if (!sideNav.contains(event.target) && !menuBtn.contains(event.target)) {
-        sideNav.style.right = "-250px";
-        menuBtn.style.background = "rgba(165, 199, 181, 0.9)";
-        menu.src = "images/menu.png";
-    }
+menuBtn.addEventListener("mouseleave", () => {
+    setTimeout(() => {
+        if (!sideNav.matches(":hover")) {
+            sideNav.style.right = "-250px";
+            menuBtn.style.background = "rgba(165, 199, 181, 0.9)";
+            menu.src = "images/menu.png";
+            menuBtn.style.display = "block";
+        }
+    }, 200);
+});
+
+sideNav.addEventListener("mouseleave", () => {
+    sideNav.style.right = "-250px";
+    menuBtn.style.background = "rgba(165, 199, 181, 0.9)";
+
+    menuBtn.style.display = "block";
 });
